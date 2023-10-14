307a8821fb951ddd409dccbe0d5eff12dc553a69
inrl-official <fasweehmon2@gmail.com>
2023-10-14 10:20:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 function check(word){
-            let content = fs.readFileSync(__dirname+'en.txt');
+            let content = fs.readFileSync('./en.txt');
             let regex = new RegExp('\n' + word +'\n');
             if (content.toString('utf-8').match(regex)) {
                 return true;

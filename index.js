58226bdc03fce9100c020d72c0658280a4f5f226
inrl-official <fasweehmon2@gmail.com>
2023-10-13 22:56:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const fs = require('fs');
-function check(word){
-            let content = fs.readFileSync('./en.txt');
-            let regex = new RegExp('\n' + word +'\n');
-            if (content.toString('utf-8').match(regex)) {
-                return true;
-            }
-            return false;
-        }
-module.exports ={check};
+const languageRegex = require(`./en.js`);
+module.exports = function check(word) {
+    return languageRegex.test(word)
+};
